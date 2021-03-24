47ff62d04b8399ba8c588c6df4aa5acdf0cd10ca
ConorDavenport <cnrdavenport@gmail.com>
2021-03-24 12:36:15 +0000

    delete commented out code

--- parserExperimental.py
+++ parserExperimental.py
@@ -11,38 +11,6 @@ def main():
   data_string_unicode = data_string.encode('utf-8')
   with open("markers", "w") as f:
     f.write(data_string_unicode)
-  #   data_as_int = list(data)
-  #   for i in range(len(data_as_int)):
-  #     if ((data_as_int[i] < 32 or data_as_int[i] > 126) and data_as_int[i] != 10):
-  #       data_as_int[i] = 0
-
-  #   for i in range(len(data_as_int)-1, -1, -1):
-  #     if (data_as_int[i] == 0):
-  #       del data_as_int[i]
-  
-  # s = "".join([chr(c) for c in data_as_int])
-
-  # with open("test.txt", "w") as b:
-  #   b.write('this is a test')
-  #   b.close()
-  
-  # with open("test.txt", "r") as b:
-  #   print(b.read())
-  #   b.close()
-
-  # f = open("markers", "w")
-  # if f.closed:
-  #   print('f is not open')
-  #   return -1
-  # else:
-  #   print('f is open')
-  # f.write(s)
-  # f.close()
-  # if f.closed:
-  #   print('f is not open')
-  #   return -1
-  # else:
-  #   print('f is open')
 
 if __name__ == "__main__":
   main()
