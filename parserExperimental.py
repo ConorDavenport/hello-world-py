8b90f8fd11b810595a3df0358585c2c66131b195
ConorDavenport <cnrdavenport@gmail.com>
2021-03-24 09:59:36 +0000

    read data and print all legal characters

--- parserExperimental.py
+++ parserExperimental.py
@@ -6,8 +6,7 @@ def main():
     data = f.read()
     for character in data:
       if ((character >= 32 and character <= 127) or character == 10):
-        data_string += character
-
+        print(character)
   #   data_as_int = list(data)
   #   for i in range(len(data_as_int)):
   #     if ((data_as_int[i] < 32 or data_as_int[i] > 126) and data_as_int[i] != 10):
