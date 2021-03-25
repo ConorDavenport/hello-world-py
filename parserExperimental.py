c56f876f202f05405fd701c84cd7ed464b55f86b
ConorDavenport <cnrdavenport@gmail.com>
2021-03-25 13:11:12 +0000

    include filepath and line number extraction for errors

--- parserExperimental.py
+++ parserExperimental.py
@@ -15,16 +15,18 @@ def main():
 
   with open ("markers", "r") as markers:
     for line in markers.readlines():
-      if ('TODO' in line and 'mpfs_hal' in line):
-        matches = line.split(r'TODO')
-        todo_info = matches[0]
-        file_path = re.search(r'mpfs.*(?=org)', todo_info)
-        line_number = re.search(r'(?<=lineNumber).*?(?=\/mpfs)', todo_info).group()
+      if (('TODO' in line or 'fatal error' in line) and 'mpfs_hal' in line):
+        if 'TODO' in line:
+          matches = line.split(r'TODO')
+        elif 'fatal error' in line:
+          matches = line.split(r'fatal error')
+
+        message_info = matches[0]
+        file_path = re.search(r'mpfs.*(?=org)', message_info)
+        line_number = re.search(r'(?<=lineNumber).*?(?=\/mpfs)', message_info).group()
         print(file_path.group() + '[' + line_number + ']')
-        todo_message = matches[1]
-        print(todo_message)
-      if ('fatal error' in line):
-        print(line)
+        message = matches[1]
+        print(message)
 
 if __name__ == "__main__":
   main()
