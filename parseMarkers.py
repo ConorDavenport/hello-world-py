7b6b8074fc8cf3bd124dd0840ca868dc5d79424e
ConorDavenport <cnrdavenport@gmail.com>
2021-04-09 09:37:17 +0100

    comment out processing

--- parseMarkers.py
+++ parseMarkers.py
@@ -25,64 +25,64 @@ def main():
     data = f.read()
     for character in data:
       if ((character >= 32 and character <= 127) or character == 10):
-        data_string += str(character).decode('utf-8')
+        data_string += str(character).decode('utf-8').encode('utf-8')
   print(data_string)
-  data_string_unicode = data_string.encode('utf-8')
-  with open("markers.json", "w") as f:
-    clean_data = []
-    prev_file_path = ''
-    for line in data_string_unicode.splitlines():
-      if ('TODO' in line):
-      # if ('TODO' in line and platform_entry in line):
-        matches = line.split(r'TODO ')
+  # data_string_unicode = data_string.decode('utf-8').encode('utf-8')
+  # with open("markers.json", "w") as f:
+  #   clean_data = []
+  #   prev_file_path = ''
+  #   for line in data_string_unicode.splitlines():
+  #     if ('TODO' in line):
+  #     # if ('TODO' in line and platform_entry in line):
+  #       matches = line.split(r'TODO ')
 
-        todo_info = matches[0]
+  #       todo_info = matches[0]
 
-        file_path = getFilePath(prev_file_path, todo_info)
-        if file_path != prev_file_path: prev_file_path = file_path
+  #       file_path = getFilePath(prev_file_path, todo_info)
+  #       if file_path != prev_file_path: prev_file_path = file_path
 
-        line_number = ''
-        search_line_number = re.search(r'(?<=lineNumber).*?(?=\/mpfs)', todo_info)
-        if search_line_number != None:
-          line_number = search_line_number.group()
-        else:
-          search_line_number = re.search(r'(?<=lineNumber).*?(?=source)', todo_info)
-          if search_line_number != None:
-            line_number = search_line_number.group()
+  #       line_number = ''
+  #       search_line_number = re.search(r'(?<=lineNumber).*?(?=\/mpfs)', todo_info)
+  #       if search_line_number != None:
+  #         line_number = search_line_number.group()
+  #       else:
+  #         search_line_number = re.search(r'(?<=lineNumber).*?(?=source)', todo_info)
+  #         if search_line_number != None:
+  #           line_number = search_line_number.group()
 
-        todo_path = 'TODO ' + file_path + '[' + line_number + ']'
-        print(todo_path)
+  #       todo_path = 'TODO ' + file_path + '[' + line_number + ']'
+  #       print(todo_path)
 
-        todo_message = matches[1]
-        if re.search(r'priority$', todo_message) != None:
-          todo_message = re.split(r'priority$', todo_message)[0]
+  #       todo_message = matches[1]
+  #       if re.search(r'priority$', todo_message) != None:
+  #         todo_message = re.split(r'priority$', todo_message)[0]
 
-        print(todo_message)
+  #       print(todo_message)
 
-        clean_data.append({'path':todo_path, 'message':todo_message})
+  #       clean_data.append({'path':todo_path, 'message':todo_message})
 
-      if ('fatal error' in line):
-        matches = line.split(r'fatal error: ')
+  #     if ('fatal error' in line):
+  #       matches = line.split(r'fatal error: ')
 
-        error_info = matches[0]
+  #       error_info = matches[0]
 
-        file_path = getFilePath(prev_file_path, error_info)
-        if file_path != prev_file_path: prev_file_path = file_path
+  #       file_path = getFilePath(prev_file_path, error_info)
+  #       if file_path != prev_file_path: prev_file_path = file_path
 
-        line_number = ''
-        search_line_number = re.search(r'(?<=lineNumber).*?(?=\/mpfs)', error_info)
-        if search_line_number != None:
-          line_number = search_line_number.group()
+  #       line_number = ''
+  #       search_line_number = re.search(r'(?<=lineNumber).*?(?=\/mpfs)', error_info)
+  #       if search_line_number != None:
+  #         line_number = search_line_number.group()
 
-        error_path = 'ERROR ' + file_path + '[' + line_number + ']'
-        print(error_path)
+  #       error_path = 'ERROR ' + file_path + '[' + line_number + ']'
+  #       print(error_path)
 
-        error_message = matches[1]
-        print(error_message)
+  #       error_message = matches[1]
+  #       print(error_message)
 
-        clean_data.append({'path':error_path, 'message':error_message})
+  #       clean_data.append({'path':error_path, 'message':error_message})
 
-    json.dump(clean_data, f)
+  #   json.dump(clean_data, f)
 
 if __name__ == "__main__":
   main()
