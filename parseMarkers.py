0d8e133bd211c1ca2a72d477ab7c7d9f6d87e577
ConorDavenport <cnrdavenport@gmail.com>
2021-04-09 09:28:01 +0100

    print character

--- parseMarkers.py
+++ parseMarkers.py
@@ -24,6 +24,7 @@ def main():
   with open(sys.argv[1], "rb") as f:
     data = f.read()
     for character in data:
+      print(character)
       character_byte = bytes(character,'utf-8')
       if ((character_byte >= b'\x20' and character_byte <= b'\x7F') or character_byte == b'\x0A'):
         data_string += character
