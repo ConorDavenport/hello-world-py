9735ebfe5ede49ec5fa0c6d530fb588f16e9fb6c
ConorDavenport <cnrdavenport@gmail.com>
2021-04-12 11:05:30 +0100

    copy script that works in python3

--- parseMarkers.py
+++ parseMarkers.py
@@ -4,6 +4,7 @@ import json
 
 # get the file path for the todo/error
 def getFilePath(prev_file_path, info):
+  file_path = prev_file_path
   search_file_path = re.search(r'mpfs.*?\..', info)
   if search_file_path != None:
     file_path = search_file_path.group()
@@ -16,6 +17,9 @@ def getFilePath(prev_file_path, info):
 
 def main():
   sys.stdout.flush()
+  # search json file for the name of the repo currently being
+  # tested in the pipeline. Get the name of the corresponding
+  # entry in platform
   platform_entry = ""
   with open("bitbucket_project_description.json") as f:
     data = json.load(f)
@@ -39,10 +43,12 @@ def main():
     formatted_data = []
     prev_file_path = ''
     for line in clean_data:
+      # filter out only todos that are revelant to this repo
       if ('TODO' in line and platform_entry in line):
         matches = line.split(r'TODO ')
 
         todo_info = matches[0]
+
         file_path = getFilePath(prev_file_path, todo_info)
         if file_path != prev_file_path: prev_file_path = file_path
 
@@ -57,6 +63,7 @@ def main():
 
         todo_path = 'TODO ' + file_path + '[' + line_number + ']'
         print(todo_path)
+
         # remove trailing 'priority' from todo message
         todo_message = matches[1]
         if re.search(r'priority$', todo_message) != None:
@@ -81,11 +88,13 @@ def main():
 
         error_path = 'ERROR ' + file_path + '[' + line_number + ']'
         print(error_path)
+
         error_message = matches[1]
         print(error_message)
+
         formatted_data.append({'path':error_path, 'message':error_message})
 
-    json.dump(formatted_data, f)
+    json.dump(clean_data, f)
 
 if __name__ == "__main__":
   main()
