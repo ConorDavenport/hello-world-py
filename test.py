d4988931774ae80822c5030dfb59e80215b6d006
ConorDavenport <cnrdavenport@gmail.com>
2021-04-13 12:46:24 +0100

    test script

---
+++ test.py
@@ -0,0 +1,8 @@
+import re
+
+def main():
+  jobURL = 'https://ci.devops.microchip.com/job/FPGA_PFSOC_ES/job/mss-usb-examples/job/develop-ci-testing-report/48/'
+  print(re.search(r'\d*\/$',jobURL)[0][:-1])
+
+if __name__ == "__main__":
+    main()
