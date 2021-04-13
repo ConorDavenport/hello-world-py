caaa7bfc2b95a3a73a187753d8f595d10061c3d2
ConorDavenport <cnrdavenport@gmail.com>
2021-04-13 12:53:02 +0100

    get first match

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import re
 
 def main():
   jobURL = 'https://ci.devops.microchip.com/job/FPGA_PFSOC_ES/job/mss-usb-examples/job/develop-ci-testing-report/48/'
-  print(re.search(r'\d*\/$',jobURL))
+  print(re.search(r'\d*\/$',jobURL).group(0))
+
 
 if __name__ == "__main__":
     main()
