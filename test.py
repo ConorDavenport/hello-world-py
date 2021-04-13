e7e0ebc051fc5f4c7f9b8eed68e8e2bc401f5228
ConorDavenport <cnrdavenport@gmail.com>
2021-04-13 12:53:37 +0100

    remove final character

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import re
 
 def main():
   jobURL = 'https://ci.devops.microchip.com/job/FPGA_PFSOC_ES/job/mss-usb-examples/job/develop-ci-testing-report/48/'
-  print(re.search(r'\d*\/$',jobURL).group(0))
+  print(re.search(r'\d*\/$',jobURL).group(0)[:-1])
 
 
 if __name__ == "__main__":
