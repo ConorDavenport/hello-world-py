0af88003f32ce1356861b6bf6272ec48e8a82ad1
ConorDavenport <cnrdavenport@gmail.com>
2021-04-13 12:51:11 +0100

    remove selector

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import re
 
 def main():
   jobURL = 'https://ci.devops.microchip.com/job/FPGA_PFSOC_ES/job/mss-usb-examples/job/develop-ci-testing-report/48/'
-  print(re.search(r'\d*\/$',jobURL)[0])
+  print(re.search(r'\d*\/$',jobURL))
 
 if __name__ == "__main__":
     main()
