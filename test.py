b74bff53f20a3f0d8d7d9e6d8f82f433c44f6e13
ConorDavenport <cnrdavenport@gmail.com>
2021-05-21 10:03:13 +0100

    remove test script;

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def main():
-  jobURL = 'https://ci.devops.microchip.com/job/FPGA_PFSOC_ES/job/mss-usb-examples/job/develop-ci-testing-report/48/'
-  print(re.search(r'\d*\/$',jobURL).group(0)[:-1])
-
-
-if __name__ == "__main__":
-    main()
